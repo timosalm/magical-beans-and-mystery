8f4838c0f28a713b88034ff7cb93f690abc84dda
Timo Salm <52704922+timosalm@users.noreply.github.com>
2024-05-23 13:22:05 -0400

    Improvements

--- spring-framework-web-app/src/test/java/org/example/RecipeResourceTests.java
+++ spring-framework-web-app/src/test/java/org/example/RecipeResourceTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
 import org.springframework.web.context.WebApplicationContext;
@@ -13,9 +13,8 @@ import org.springframework.web.context.WebApplicationContext;
 // MockMvc is one option to test Spring MVC applications. Another option is the WebTestClient,
 // which is used here as it allows you to work with higher level objects instead of raw data,
 // and to run the same tests against a live server.
-@WebAppConfiguration
-@SpringJUnitConfig(classes = AppConfig.class)
-class RecipeResourceTesta {
+@SpringJUnitWebConfig(classes = AppConfig.class, loader = AnnotationConfigWebContextLoader.class)
+class RecipeResourceTests {
 
     @Autowired
     WebApplicationContext applicationContext;
